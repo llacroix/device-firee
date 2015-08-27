d7a175136b7368a41821832b4d414ff50d8acb97
Loïc Faure-Lacroix <lamerstar@gmail.com>
2015-08-27 23:09:50 +0200

    Base files

---
+++ BoardConfig.mk
@@ -0,0 +1,1 @@
+include device/qcom/msm8610/BoardConfig.mk
---
+++ firee.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/qcom/msm8610/msm8610.mk)
+
+PRODUCT_NAME := firee
+PRODUCT_DEVICE := firee
+PRODUCT_BRAND := qcom
+PRODUCT_MANUFACTURER := alcatel
+PRODUCT_MODEL := firee
