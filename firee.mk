777a22d3d1db33d995ce2d3897f9e79fa319b980
Loïc Faure-Lacroix <lamerstar@gmail.com>
2015-10-08 20:49:54 +0300

    Added pixels

--- firee.mk
+++ firee.mk
@@ -3,8 +3,10 @@ $(call inherit-product, device/qcom/msm8610/msm8610.mk)
 PRODUCT_COPY_FILES := \
 	device/alcatel/firee/boot.ver:root/boot.ver \
 	device/alcatel/firee/init.rc:root/init.rc \
+	device/alcatel/firee/init.target.rc:root/init.target.rc \
 	device/alcatel/firee/fstab.qcom:root/fstab.qcom \
 
+GAIA_DEV_PIXELS_PER_PX := 1.5
 
 PRODUCT_NAME := firee
 PRODUCT_DEVICE := firee
