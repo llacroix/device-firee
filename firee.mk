316272845a6db53428bba3d613600ce56e5b4673
Loïc Faure-Lacroix <lamerstar@gmail.com>
2015-10-08 16:17:50 +0300

    Updated builder

--- firee.mk
+++ firee.mk
@@ -1,5 +1,9 @@
 $(call inherit-product, device/qcom/msm8610/msm8610.mk)
 
+PRODUCT_COPY_FILES := \
+	device/alcatel/firee/fstab.qcom:root/fstab.qcom \
+
+
 PRODUCT_NAME := firee
 PRODUCT_DEVICE := firee
 PRODUCT_BRAND := qcom
